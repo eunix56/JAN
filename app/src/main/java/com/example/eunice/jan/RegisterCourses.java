a7e8887afca012d954666793d3ae2b5bfab86760
Bakare Eunice <Bakare Eunice>
2018-05-27 14:26:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/eunice/jan/RegisterCourses.java
@@ -0,0 +1,29 @@
+package com.example.eunice.jan;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterCourses extends Fragment {
+
+
+    public RegisterCourses() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_courses, container, false);
+    }
+
+}
